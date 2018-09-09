148c3cfe1ee55bfd9b0c363a6c7a80ea6f4129e7
程嘉梁 <chengjl16@163.com>
2018-09-09 18:07:59 +0800

    1.0.1 解决了物体识别视频卡顿的问题，虽然还有略微延迟，但总体比较流畅。

--- object_detect.py
+++ object_detect.py
@@ -51,6 +51,7 @@ for file in tar_file.getmembers():
 #建一个图，导入tensorflow模型
 detection_graph = tf.Graph()
 with detection_graph.as_default():
+    sess=tf.Session()
     od_graph_def = tf.GraphDef()
     with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
         serialized_graph = fid.read()
@@ -75,52 +76,51 @@ def load_image_into_numpy_array(image):
 '''
 def object_detect(image):
     with detection_graph.as_default():
-        with tf.Session() as sess:
-            ops = tf.get_default_graph().get_operations()
-            all_tensor_names = {output.name for op in ops for output in op.outputs}
-            tensor_dict = {}
-            for key in ['num_detections', 'detection_boxes', 'detection_scores',
-            'detection_classes', 'detection_masks']:
-                tensor_name = key + ':0'
-                if tensor_name in all_tensor_names:
-                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
-            if 'detection_masks' in tensor_dict:
-                #对一张图片进行处理
-                detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-                detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-                real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-                detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
-                detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
-                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(detection_masks, detection_boxes, image.shape[0], image.shape[1])
-                detection_masks_reframed = tf.cast(
-                    tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
-            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+        ops = tf.get_default_graph().get_operations()
+        all_tensor_names = {output.name for op in ops for output in op.outputs}
+        tensor_dict = {}
+        for key in ['num_detections', 'detection_boxes', 'detection_scores',
+        'detection_classes', 'detection_masks']:
+            tensor_name = key + ':0'
+            if tensor_name in all_tensor_names:
+                tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
+        if 'detection_masks' in tensor_dict:
+            #对一张图片进行处理
+            detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+            detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+            real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+            detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+            detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+            detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(detection_masks, detection_boxes, image.shape[0], image.shape[1])
+            detection_masks_reframed = tf.cast(
+                tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+            tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
+        image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-            #喂入数据
-            output_dict = sess.run(tensor_dict,
-                            feed_dict={image_tensor: np.expand_dims(image,axis=0)})
+        #喂入数据
+        output_dict = sess.run(tensor_dict,
+                        feed_dict={image_tensor: np.expand_dims(image,axis=0)})
 
-            #将输出数据转换类型
-            output_dict['num_detections'] = int(output_dict['num_detections'][0])
-            output_dict['detection_classes'] = output_dict[
-                                            'detection_classes'][0].astype(np.uint8)
-            output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-            output_dict['detection_scores'] = output_dict['detection_scores'][0]
-            if 'detection_masks' in output_dict:
-                output_dict['detection_masks'] = output_dict['detection_masks'][0]
+        #将输出数据转换类型
+        output_dict['num_detections'] = int(output_dict['num_detections'][0])
+        output_dict['detection_classes'] = output_dict[
+                                        'detection_classes'][0].astype(np.uint8)
+        output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+        output_dict['detection_scores'] = output_dict['detection_scores'][0]
+        if 'detection_masks' in output_dict:
+            output_dict['detection_masks'] = output_dict['detection_masks'][0]
 
-            vis_util.visualize_boxes_and_labels_on_image_array(
-                image,
-                output_dict['detection_boxes'],
-                output_dict['detection_classes'],
-                output_dict['detection_scores'],
-                category_index,
-                instance_masks=output_dict.get('detection_masks'),
-                use_normalized_coordinates=True,
-                line_thickness=3)
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image,
+            output_dict['detection_boxes'],
+            output_dict['detection_classes'],
+            output_dict['detection_scores'],
+            category_index,
+            instance_masks=output_dict.get('detection_masks'),
+            use_normalized_coordinates=True,
+            line_thickness=3)
 
-            return image
+        return image
 
 if __name__=='__main__':
     pass
