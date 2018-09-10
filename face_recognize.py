a5c16d740ed6ca185de2146b45268880d3b2a3af
JunguangJiang <13126830206@163.com>
2018-09-10 17:15:50 +0800

    1.0.6 根据不同罪犯等级加上不同的颜色

--- face_recognize.py
+++ face_recognize.py
@@ -21,23 +21,35 @@ class face_recognize:
             self.face_encodings = face_recognition.face_encodings(rgb_small_image, self.face_locations)
 
             self.face_ids = []
+            self.face_important = []
             for face_encoding in self.face_encodings:
                 matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding, tolerance=0.4)
                 id = "Unknown"
+                important = "C"
                 if True in matches:
                     first_match_index = matches.index(True)
                     id = self.known_face_id[first_match_index]
-                self.face_ids.append(id)
+                    important = "A" if self.know_face_important[first_match_index] else "B"
 
-        for (top, right, bottom, left), id in zip(self.face_locations, self.face_ids):
+                self.face_ids.append(id)
+                self.face_important.append(important)
+
+        for (top, right, bottom, left), id, important in zip(self.face_locations, self.face_ids, self.face_important):
             top *= int(1.0/ratio)
             right *= int(1.0/ratio)
             bottom *= int(1.0/ratio)
             left *= int(1.0/ratio)
 
-            cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
+            if important == "A":
+                color = (0,0,255)
+            elif important == "B":
+                color = (0,255,0)
+            else:
+                color = (255, 0, 0)
 
-            cv2.rectangle(image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            cv2.rectangle(image, (left, top), (right, bottom), color, 2)
+
+            cv2.rectangle(image, (left, bottom - 35), (right, bottom), color, cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(image, id, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
@@ -47,11 +59,12 @@ class face_recognize:
         '''更新罪犯数据库'''
         db = get_db()
         criminals = db.execute(
-            'SELECT id, encoding FROM criminal ORDER BY rank'
+            'SELECT id, encoding, important FROM criminal ORDER BY rank'
         )
         self.known_face_id = []
         self.known_face_encodings = []
+        self.know_face_important = []
         for criminal in criminals:
             self.known_face_id.append(criminal['id'])
             self.known_face_encodings.append(pickle.loads(criminal['encoding']))
-        # print(self.known_face_id)
+            self.know_face_important.append(criminal['important'])
